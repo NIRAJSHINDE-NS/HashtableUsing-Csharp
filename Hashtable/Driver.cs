df76738db2bec74e49cf20ad67334782ee652fd8
NIRAJ <niraj@rajniraj.com>
2023-05-23 18:03:15 +0530

    [Niraj]Added:This are the program files for hashtable implementation using c#

---
+++ Hashtable/Driver.cs
@@ -0,0 +1,46 @@
+﻿namespace Hashtable
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashTable<string, int> hashTable = new HashTable<string, int>();
+
+            // Add elements to the hashtable
+            hashTable.Add("apple", 1);
+            hashTable.Add("banana", 2);
+            hashTable.Add("cherry", 3);
+
+            // Retrieve and print values
+            int appleValue;
+            if (hashTable.TryGetValue("apple", out appleValue))
+            {
+                Console.WriteLine("Value of 'apple': " + appleValue);
+            }
+
+            int orangeValue;
+            if (hashTable.TryGetValue("orange", out orangeValue))
+            {
+                Console.WriteLine("Value of 'orange': " + orangeValue);
+            }
+            else
+            {
+                Console.WriteLine("Key 'orange' does not exist in the hashtable.");
+            }
+
+            // Remove an element
+            bool removed = hashTable.Remove("banana");
+            if (removed)
+            {
+                Console.WriteLine("Successfully removed 'banana' from the hashtable.");
+            }
+            else
+            {
+                Console.WriteLine("'banana' does not exist in the hashtable.");
+            }
+        }
+    }
+
+}
---
+++ Hashtable/HashTable.cs
@@ -0,0 +1,104 @@
+﻿namespace Hashtable
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class HashTable<TKey, TValue>
+    {
+        private const int InitialCapacity = 16;
+        private LinkedList<KeyValuePair<TKey, TValue>>[] buckets;
+
+        public HashTable()
+        {
+            buckets = new LinkedList<KeyValuePair<TKey, TValue>>[InitialCapacity];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            int bucketIndex = GetBucketIndex(key);
+            if (buckets[bucketIndex] == null)
+            {
+                buckets[bucketIndex] = new LinkedList<KeyValuePair<TKey, TValue>>();
+            }
+
+            var bucket = buckets[bucketIndex];
+            foreach (var pair in bucket)
+            {
+                if (pair.Key.Equals(key))
+                {
+                    throw new ArgumentException("An element with the same key already exists in the hashtable.");
+                }
+            }
+
+            bucket.AddLast(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            int bucketIndex = GetBucketIndex(key);
+            var bucket = buckets[bucketIndex];
+            if (bucket != null)
+            {
+                foreach (var pair in bucket)
+                {
+                    if (pair.Key.Equals(key))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int bucketIndex = GetBucketIndex(key);
+            var bucket = buckets[bucketIndex];
+            if (bucket != null)
+            {
+                foreach (var pair in bucket)
+                {
+                    if (pair.Key.Equals(key))
+                    {
+                        value = pair.Value;
+                        return true;
+                    }
+                }
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public bool Remove(TKey key)
+        {
+            int bucketIndex = GetBucketIndex(key);
+            var bucket = buckets[bucketIndex];
+            if (bucket != null)
+            {
+                var node = bucket.First;
+                while (node != null)
+                {
+                    if (node.Value.Key.Equals(key))
+                    {
+                        bucket.Remove(node);
+                        return true;
+                    }
+
+                    node = node.Next;
+                }
+            }
+
+            return false;
+        }
+
+        private int GetBucketIndex(TKey key)
+        {
+            int hashCode = key.GetHashCode();
+            int bucketIndex = hashCode % buckets.Length;
+            return Math.Abs(bucketIndex);
+        }
+    }
+
+}
